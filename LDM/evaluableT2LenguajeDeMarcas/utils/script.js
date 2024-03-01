725524c0ac7992a0e0fa9df3fde7e6326acc0b68
iordanericdam <152529718+iordanericdam@users.noreply.github.com>
2024-03-01 20:40:19 +0100

    Subida LDM

---
+++ LDM/evaluableT2LenguajeDeMarcas/utils/script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let inputNombre = document.querySelector("#inputNombre");
+  let inputApellidos = document.querySelector("#inputApellidos");
+  let inputCorreo = document.querySelector("#inputCorreo");
+  let selectDepartamento = document.querySelector("#selectDepartamento");
+  let boton = document.querySelector("#guardarDatos");
+  let lista = document.querySelector("#listaUsuarios");
+
+  let parrafoNombre = document.querySelector("#parrafoNombre");
+  let parrafoApellidos = document.querySelector("#parrafoApellidos");
+  let parrafoCorreo = document.querySelector("#parrafoCorreo");
+  let pDepartamento = document.querySelector("#parrafoDepartamento");
+
+  boton.addEventListener("click", (e) => {
+    if (
+      inputNombre.value.length > 0 &&
+      inputApellidos.value.length > 0 &&
+      inputCorreo.value.length > 0
+    ) {
+      /* parrafoNombre.textContent = inputNombre.value;
+      parrafoApellidos.textContent = inputApellidos.value;
+      parrafoCorreo.textContent = inputCorreo.value;
+      pDepartamento.textContent = selectDepartamento.value; */
+
+      let nodo = document.createElement("li");
+      nodo.textContent = `${inputNombre.value} ${inputApellidos.value}`;
+      nodo.className = "list-group-item";
+      lista.append(nodo);
+
+      inputNombre.value = "";
+      inputApellidos.value = "";
+    }
+  });
+});
