339d7a8b71cd0324c36e766481539a5babd423c9
iordanericdam <152529718+iordanericdam@users.noreply.github.com>
2024-03-24 20:01:08 +0100

    70%
    Clases Articulo Revista Libro y Pelicula creadas y terminadas, método para añadir l,p,r terminado.
    Falta:
    -Control de datos en el metodo altaArticulo
    -Resto de cosas.

--- Programacion/gestionBiblioteca/src/gestionBiblioteca/main.java
+++ Programacion/gestionBiblioteca/src/gestionBiblioteca/main.java
@@ -20,12 +20,15 @@ package gestionBiblioteca;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class main {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		ArrayList<Usuario> listaUsuarios = new ArrayList<Usuario>();
+		ArrayList<Articulo> listaArticulos = new ArrayList<Articulo>();
+		Articulo art = new Articulo();
 
 //		Creacion de usuarios de administrador
 		Administrador admin1 = new Administrador("admin", "admin", "46660198Y", "Admin");
@@ -37,6 +40,32 @@ public class main {
 		listaUsuarios.add(usuario1);
 		Usuario usuario2 = new Usuario("david", "david", "00188202Q", "David Gil", false);
 		listaUsuarios.add(usuario2);
+		
+        // Crear libros
+        Libro libro1 = new Libro(14, "Español", "Cien años de soledad", true, 4652, "Gabriel García Márquez", 4);
+        Libro libro2 = new Libro(21, "Inglés", "To Kill a Mockingbird", true, 36524, "Harper Lee", 12);
+        Libro libro3 = new Libro(10, "Francés", "Le Petit Prince", true, 789651, "Antoine de Saint-Exupéry", 5);
+
+        // Crear revistas
+        Revista revista1 = new Revista(7, "Español", "National Geographic", true, 112255, "National Geographic Society", 1);
+        Revista revista2 = new Revista(7, "Inglés", "Scientific American", true, 885533, "Springer Nature", 2);
+        Revista revista3 = new Revista(7, "Francés", "Paris Match", true, 99966, "Lagarde Family", 6);
+
+        // Crear películas
+        Pelicula pelicula1 = new Pelicula(3, "Inglés", "The Shawshank Redemption", true, "Frank Darabont", 5, 6);
+        Pelicula pelicula2 = new Pelicula(3, "Español", "El Laberinto del Fauno", true, "Guillermo del Toro", 2, 3);
+        Pelicula pelicula3 = new Pelicula(3, "Inglés", "Inception", true, "Christopher Nolan", 4, 55);
+
+        // Añadir los artículos a la lista
+        listaArticulos.add(libro1);
+        listaArticulos.add(libro2);
+        listaArticulos.add(libro3);
+        listaArticulos.add(revista1);
+        listaArticulos.add(revista2);
+        listaArticulos.add(revista3);
+        listaArticulos.add(pelicula1);
+        listaArticulos.add(pelicula2);
+        listaArticulos.add(pelicula3);
 
 		Usuario usu = new Usuario();
 
@@ -67,6 +96,15 @@ public class main {
 						Usuario.consultarUsuarios(listaUsuarios);
 						break;
 					case 4:
+						do {
+							Articulo.mostrarMenuArticulos();
+							opcionS = sc.nextLine();
+							opcion = comprobarSiNumero(opcionS);
+							if (opcion != 4) {
+								Articulo.altaArticulo(opcion, listaArticulos, art, sc);
+							}
+						} while (opcion != 4);
+						
 						break;
 					case 5:
 						break;
@@ -74,6 +112,10 @@ public class main {
 						break;
 					case 7:
 						break;
+					case 8:
+						Articulo.consultarArticulos(listaArticulos);
+						
+						break;	
 					}
 				} while (opcion != 7);
 			
@@ -108,13 +150,7 @@ public class main {
 		} while (true);
 	}
 	
-	
-	
-	
-	
-	
-	
-	
+
 	
 
 	protected static int comprobarSiNumero(String numeroS) {
